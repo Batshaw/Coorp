646bb50a092cf0a09e6567d84fcb43fa6faa0c1f
Batshaw <quang.hafenstadt@gmail.com>
2018-07-09 15:17:21 +0200

    last change

--- source/test2.cpp
+++ source/test2.cpp
@@ -5,10 +5,11 @@
 #include "Box.hpp"
 #include "material.hpp"
 
+using namespace std;
+
 TEST_CASE("aufgabe3", "[interect]")
 {
     Ray ray;
-<<<<<<< HEAD
     ray.origin = {0.0,0.0,-8.0};
     ray.direction = {1, 1, -1.0};
     float distance = 0;
@@ -18,22 +19,7 @@ TEST_CASE("aufgabe3", "[interect]")
 
     Box box1{v1, v2};
 
-    cout<<box1;
-    REQUIRE(box1.intersect(ray, distance) == true);
-
-}
-=======
-    ray.origin = {0.0, 0.0, -8.0};
-    ray.direction = {1, 1, -1.0};
-
-    float distance = 0;
-
-    vec3 v1{-1.5, -7.6, -1.5};
-    vec3 v2{4.5, 6.7, -7.0};
-    Box box1{v1, v2};
->>>>>>> 847b38725f134112943ab21dd8f30a706e6555aa
-
-    cout << box1;
+    std::cout<<box1;
     REQUIRE(box1.intersect(ray, distance) == true);
 }
 
