ee24b5a467b9e87c079f130ffc3ca72b4b78aa8f
Batshaw <quang.hafenstadt@gmail.com>
2018-07-09 15:02:33 +0200

    remove getch()

--- source/test2.cpp
+++ source/test2.cpp
@@ -4,25 +4,23 @@
 #include "Shape.hpp"
 #include "Box.hpp"
 #include "material.hpp"
-#include <conio.h>
 
-// TEST_CASE("aufgabe3", "[interect]")
-// {
-//     Ray ray;
-//     ray.origin = {0.0,0.0,-8.0};
-//     ray.direction = {1, 1, -1.0};
+TEST_CASE("aufgabe3", "[interect]")
+{
+    Ray ray;
+    ray.origin = {0.0,0.0,-8.0};
+    ray.direction = {1, 1, -1.0};
+    float distance = 0;
 
-//     float distance = 0;
+    vec3 v1{-1.5, -7.6, -1.5};
+    vec3 v2{4.5, 6.7, -7.0};
 
-//     vec3 v1{-1.5, -7.6, -1.5};
-//     vec3 v2{4.5, 6.7, -7.0};
+    Box box1{v1, v2};
 
-//     Box box1{v1, v2};
+    cout<<box1;
+    REQUIRE(box1.intersect(ray, distance) == true);
 
-//     cout<<box1;
-//     REQUIRE(box1.intersect(ray, distance) == true);
-
-// }
+}
 
 
 TEST_CASE("aufgabe5", "[scene]")
@@ -38,6 +36,5 @@ TEST_CASE("aufgabe5", "[scene]")
 
 int main(int argc, char *argv[])
 {
-    getch();
     return Catch::Session().run(argc, argv);
 }
