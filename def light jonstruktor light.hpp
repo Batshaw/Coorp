de439669ee2feae66dcc34109992a3e7110aa104
Lucky Chandrautama <38219434+luckychandrautama@users.noreply.github.com>
2018-08-15 11:07:46 +0200

    Create def light jonstruktor light.hpp

---
+++ def light jonstruktor light.hpp
@@ -0,0 +1,5 @@
+       Light() : _name{"default Light"},
+              _origin{10.0f, 20.0f, 10.0f},
+              _color{1.0f, 1.0f, 1.0f},
+              _brightness{2000.0f} 
+              {};
